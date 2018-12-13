d4479a6fe8511e7d3411ff2e9045c98728e8d4b3
Max Alva <alva85@gmail.com>
2018-12-12 22:28:04 -0500

    First commit

---
+++ src/App/Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <img src="/assets/img/logo.png" alt="" />
+        </header>
+    );
+};
+
+export default Header;
