f3e6bbda4bd9e4765608e0ece4520fa4a11c9589
Max Alva <alva85@gmail.com>
2018-12-13 20:30:21 -0500

    Sección nosotros y contacto

--- src/App/Components/Pages/About/About.tsx
+++ src/App/Components/Pages/About/About.tsx
@@ -3,8 +3,14 @@ import './About.css';
 
 const About = () => {
     return (
-        <div>
-            <h1>Nosotros</h1>
+        <div className="contenedor-nosotros">
+            <div className="imagen">
+                <img src="/assets/img/camisa_1.png" alt="Imagen nosotros" />
+            </div>
+            <div className="contenido">
+                <h2>Sobre Nosotros</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis possimus ipsum non consequuntur atque obcaecati perferendis nihil, odio voluptatem labore deleniti eaque a veritatis necessitatibus sapiente sit voluptatibus dolorum sed.</p>
+            </div>
         </div>
     );
 };
--- src/App/Components/Pages/Contact/Contact.tsx
+++ src/App/Components/Pages/Contact/Contact.tsx
@@ -3,9 +3,24 @@ import './Contact.css';
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact</h1>
-        </div>
+        <form>
+            <legend>Formulario de Contacto</legend>
+            <div className="input-field">
+                <label htmlFor="name">Nombre: </label>
+                <input type="text" id="name" placeholder="Tu nombre" />
+            </div>
+            <div className="input-field">
+                <label htmlFor="email">Email: </label>
+                <input type="email" id="email" placeholder="Tu Email" />
+            </div>
+            <div className="input-field">
+                <label htmlFor="message">Mensaje: </label>
+                <textarea id="message" placeholder="Tu mensaje" />
+            </div>
+            <div className="input-field enviar">
+                <input type="submit" value="Enviar" />
+            </div>
+        </form>
     );
 };
 
